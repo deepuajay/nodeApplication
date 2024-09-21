8b2c5cae407541161a03ce944a0dc2ceaaaf7e22
Deepu Ajay <deepuajay383@gmail.com>
2024-09-21 11:55:35 -0400

    post and get coded in index.js

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ let datastore = {
 app.get("/users", (req, res) => {
   res.json(datastore.users);
 });
+
+app.post("/user", (req, res) => {
+  const userData = req.body;
+  if (!userData || !userData.username || !userData.age) {
+    res.status(400).json({ error: "Invalid Data" });
+  }
+  datastore.users.push(userData);
+  res.status(201).json(userData);
+});
+
+app.listen(port, () => console.log("Server is listening to port 3000."));
